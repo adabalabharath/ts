5a909170fb5a662751c87a13f2e77a42eba39e26
adabalabharath <saibharath.adabala@gmail.com>
2024-10-28 17:07:59 +0530

    ts dive

--- build/js/main.js
+++ build/js/main.js
@@ -4,5 +4,33 @@ console.log(username);
 let a = 12;
 let b = '6';
 let c = 2;
-console.log(a / b);
-console.log(c * b);
+let Str = ['hi', 'hello', 'welcome'];
+let mixed = ['oh', 'ushh', 56];
+//Str=mixed (couldnt add)
+mixed = Str;
+//array type
+let tup = ['he', 34, true];
+let alb = ['f', 5];
+let g = {
+    name: 'dave',
+    albums: 5
+};
+//literal types
+let user;
+user = 'justin';
+const logMsg = (message) => {
+    console.log(message);
+};
+const add = (a, b) => {
+    return a + b;
+};
+// interface math{
+//     (a:number,b:number,c?:number):number
+// }
+const addAll = (a, b, c) => {
+    if (c)
+        return a + b + c;
+    return a + b;
+};
+logMsg(add(3, 4));
+logMsg(addAll(35, 5));
--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,59 @@ let a=12
 let b='6'
 let c=2
 
-console.log(a/b)
-console.log(c*b)
+let Str=['hi','hello','welcome']
+
+let mixed=['oh','ushh',56]
+
+//Str=mixed (couldnt add)
+mixed=Str
+
+//array type
+let tup:[string,number,boolean]=['he',34,true]
+
+let alb:(string|number)[]=['f',5]
+
+
+//object type
+type Guitarist={
+    name:string,
+    albums:string|number
+}
+let g:Guitarist={
+    name:'dave',
+    albums:5
+}
+
+//literal types
+let user:'dave'|'harry'|'justin'
+user='justin'
+
+
+const logMsg=(message:any):void=>{
+    console.log(message)
+}
+const add=(a:number,b:number):number=>{
+    return a+b;
+}
+
+//optional params
+// const addAll=(a:number=2,b:number,c?:number):number=>{
+//   if(c)
+//    return a+b+c;
+//   return a+b
+// }
+
+ type math=(a:number,b:number,c?:number)=>number
+
+// interface math{
+//     (a:number,b:number,c?:number):number
+// }
+const addAll:math=(a,b,c):number=>{
+  if(c)
+   return a+b+c;
+  return a+b
+}
+
+
+logMsg(add(3,4))
+logMsg(addAll(35,5))
